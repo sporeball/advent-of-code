4d8a36f9b0c5824615b7534100d3294a0307bbd9
sporeball <sporeballdev@gmail.com>
2019-12-04 17:11:31 -0800

    complete day 1

---
+++ src/01-the-tyranny-of-the-rocket-equation/fcu2.js
@@ -0,0 +1,18 @@
+const fcu2 = (input) => {
+  var arr = input.split("\n");
+  var fuel = 0;
+  
+  for (var i = 0; i < arr.length; i++) {
+    var n = Number(arr[i]);
+    
+    var sub = Math.floor(n / 3) - 2;
+    while (sub > 0) {
+      fuel += sub;
+      sub = Math.floor(sub / 3) - 2; // divide up previous
+    }
+  }
+  
+  return fuel;
+}
+
+module.exports = fcu2;
--- src/01-the-tyranny-of-the-rocket-equation/test.js
+++ src/01-the-tyranny-of-the-rocket-equation/test.js
@@ -1,7 +1,10 @@
 var fcu = require("./fcu");
+var fcu2 = require("./fcu2");
+
 var fs = require("fs");
 var path = require("path");
 var inputPath = path.join(__dirname, "input.txt");
 
 var input = fs.readFileSync(inputPath, {encoding: "utf-8"}, function(){});
-console.log(fcu(input));
+console.log("starting fuel requirement: " + fcu(input));
+console.log("sum of all (calculable) fuel requirements: " + fcu2(input));
