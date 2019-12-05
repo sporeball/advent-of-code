2fa309bbcd5e538924e70ae6af8c8438157f4a0f
sporeball <sporeballdev@gmail.com>
2019-12-05 10:14:12 -0800

    complete day 2

--- src/02-1202-program-alarm/interpreter2.js
+++ src/02-1202-program-alarm/interpreter2.js
@@ -1,11 +1,40 @@
 const interpreter2 = (input) => {
   var arr = input.split(","); // raw array from input
-  var prog = arr.map(Number); // all array elements as integers
-  var mem = prog; // computer's memory - starts as a duplicate of prog
   
+  var prog = arr.map(Number); // all array elements as integers
+  
+  var n = 0;
+  var v = 0;
+  var stop = false; // halt flag
+  
+  for (var i = 0; i < 100; i++) {
+    for (var j = 0; j < 100; j++) {
+      prog = arr.map(Number); // reset
+      
+      var mem = prog; // computer's memory - starts as a duplicate of prog
+      mem[1] = i;
+      mem[2] = j;
+      
+      var result = parse(prog, mem);
+      if (result == 19690720) {
+        n = i;
+        v = j;
+        stop = true;
+        break;
+      }
+    }
+    if (stop) {
+      break;
+    }
+  }
+  
+  return (100 * n) + v;
+}
+
+function parse(prog, mem) {
   var ptr = 0;
   
-  for (var i = 0; i < prog.length; i++) {
+  for (int of prog) {
     if (prog[ptr] == 1) {
       mem[prog[ptr + 3]] = mem[prog[ptr + 1]] + mem[prog[ptr + 2]];
     } else if (prog[ptr] == 2) {
--- src/02-1202-program-alarm/test.js
+++ src/02-1202-program-alarm/test.js
@@ -6,7 +6,8 @@ var path = require("path");
 var inputPath = path.join(__dirname, "input.txt");
 
 var input = fs.readFileSync(inputPath, {encoding: "utf-8"}, function(){});
-console.log("running Intcode interpreter v1...")
+console.log("running Intcode interpreter v1.")
 console.log("program interpreted.\nthe value at position 0 is " + interpreter(input) + ".\n");
-console.log("running Intcode interpreter v2...")
-console.log("program interpreted.\nthe value at position 0 is " + interpreter2(input) + ".");
+console.log("running Intcode interpreter v2.")
+console.log("testing nouns and verbs for a return value of 19690720...")
+console.log("program interpreted.\nthe target value was found.\n100 * noun + verb = " + interpreter2(input) + ".");
