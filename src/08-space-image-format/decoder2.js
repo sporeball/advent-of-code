e81a05ea8c2027fb71ace5b80d7969d8688f0607
sporeball <sporeballdev@gmail.com>
2019-12-07 22:27:59 -0800

    complete day 8

---
+++ src/08-space-image-format/decoder2.js
@@ -0,0 +1,34 @@
+const decoder2 = (input) => {
+  var layers = input.match(/.{1,150}/g);
+  var data = [];
+  var ptr = 0;
+  
+  for (var i = 0; i < 150; i++) {
+    for (var j = 0; j < layers.length; j++) {
+      if (layers[j].slice(i, i + 1) == 0 || layers[j].slice(i, i + 1) == 1) {
+        data.push(layers[j].slice(i, i + 1));
+        break;
+      }
+    }
+  }
+  
+  for (var i = 0; i < 6; i++) {
+    var row = "";
+    for (var j = 0; j < 25; j++) {
+      if (data[ptr] == 0) {
+        row += "#";
+      } else if (data[ptr] == 1) {
+        row += " ";
+      } else {
+        row += "~";
+      }
+      
+      ptr++;
+    }
+    console.log(row);
+  }
+  
+  return;
+}
+
+module.exports = decoder2;
--- src/08-space-image-format/test.js
+++ src/08-space-image-format/test.js
@@ -1,4 +1,5 @@
 var decoder = require("./decoder");
+var decoder2 = require("./decoder2");
 
 var fs = require("fs");
 var path = require("path");
@@ -7,3 +8,5 @@ var inputPath = path.join(__dirname, "input.txt");
 var input = fs.readFileSync(inputPath, {encoding: "utf-8"}, function(){});
 console.log("running the decoder...");
 console.log("finished.\npass the number " + decoder(input) + " along to the elves.")
+console.log("here is the decoded image:")
+decoder2(input);
