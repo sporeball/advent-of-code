33bbdae1f420273e67f4acedb5e24d390ca194c2
sporeball <sporeballdev@gmail.com>
2020-12-03 22:33:06 -0800

    more nitpicking

--- src/2020/04-passport-processing/scanner.js
+++ src/2020/04-passport-processing/scanner.js
@@ -2,6 +2,7 @@ const input = require("../../../common").input;
 
 const scanner = () => {
   var valid = 0;
+
   const passports = input.split("\n")
     .map(x => x == "" ? "X" : x) // replace blanks with X
     .join(" ") // join all
@@ -9,9 +10,12 @@ const scanner = () => {
     // turn each into array, split on field, and split every field on its colon
     .map(x => x.trim().split(" ").map(y => y.split(":")))
     .map(x => Object.fromEntries(x)); // turn each into object
+
   for (passport of passports) {
+    // passport must be 7 properties long if cid is absent, and 8 if present
     if (Object.entries(passport).length == 7 + (passport.cid !== undefined)) valid++;
   }
+  
   return valid;
 }
 
--- src/2020/04-passport-processing/scanner2.js
+++ src/2020/04-passport-processing/scanner2.js
@@ -4,7 +4,6 @@ const scanner2 = () => {
   const ecls = ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"];
   var valid = 0;
 
-
   const passports = input.split("\n")
     .map(x => x == "" ? "X" : x) // replace blanks with X
     .join(" ") // join all
@@ -20,7 +19,7 @@ const scanner2 = () => {
       passport.hcl && passport.hcl.match(/^#[0-9a-f]{6}$/) &&
       ecls.includes(passport.ecl) &&
       (passport.pid + "").length == 9 &&
-      Object.entries(passport).length == 7 + (passport.cid !== undefined)) {
+      Object.entries(passport).length == 7 + (passport.cid !== undefined)) { // passport must be 7 properties long if cid is absent, and 8 if present
       let height = +passport.hgt.slice(0, -2);
       switch (passport.hgt.slice(-2)) {
         case "cm":
@@ -31,6 +30,7 @@ const scanner2 = () => {
       }
     }
   }
+
   return valid;
 }
 
